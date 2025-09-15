87295d3d8612b315015b4b5c035dd40aae365237
Thato402 <thatoletsoela89@gmail.com>
2025-09-15 17:41:18 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -289,7 +289,7 @@ app.use((req, res) => {
 });
 
 // ---- Start Server ----
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 async function startServer() {
   try {
